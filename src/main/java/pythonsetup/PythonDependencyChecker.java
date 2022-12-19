9e2d6fb9efe2ba9525228f7a6978b4d459ed6113
Lin0pan <janek.speit@gmail.com>
2022-12-19 17:36:08 +0100

    add method to check requirements from file

--- src/main/java/pythonsetup/PythonDependencyChecker.java
+++ src/main/java/pythonsetup/PythonDependencyChecker.java
@@ -2,12 +2,36 @@ package pythonsetup;
 
 import org.nd4j.python4j.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PythonDependencyChecker {
     private PythonDependencyChecker(){
 
+    }
+    private static Map<String, String> readRequirements(String requirementsFilePath){
+        Map<String, String> requirementsMap = new HashMap<>();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    requirementsFilePath));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] module = line.split("==");
+                if(module.length > 1) {
+                    requirementsMap.put(module[0], module[1]);
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return requirementsMap;
     }
     public static String getPythonVersion(){
         try(PythonGIL pythonGIL = PythonGIL.lock()) {
@@ -24,7 +48,21 @@ public class PythonDependencyChecker {
             }
         }
     }
-
+    public static String getPipVersion(){
+        try(PythonGIL pythonGIL = PythonGIL.lock()) {
+            try(PythonGC gc = PythonGC.watch()) {
+                PythonVariable<String> out = new PythonVariable<>("version", PythonTypes.STR);
+                PythonExecutioner.exec(
+                        """
+                                import pip
+                                version = pip.__version__
+                                """,
+                        null, Collections.singletonList(out)
+                );
+                return out.getValue();
+            }
+        }
+    }
     public static Map<String, String> getAvailableLibraries() {
         try (PythonGIL pythonGIL = PythonGIL.lock()) {
             try (PythonGC gc = PythonGC.watch()) {
@@ -41,21 +79,38 @@ public class PythonDependencyChecker {
         }
     }
 
-    public static boolean versionSatisfied(String minimumVersion, String actualVersion){
+    public static boolean minVersionSatisfied(String minVersion, String actualVersion){
 
         String[] actualVersionSplit = actualVersion.split("\\.");
-        String[] minimumVersionSplit = minimumVersion.split("\\.");
+        String[] minVersionSplit = minVersion.split("\\.");
 
-        if(actualVersionSplit.length != minimumVersionSplit.length){
+        if(actualVersionSplit.length != minVersionSplit.length){
             throw new IllegalArgumentException();
         }
 
         for(int i = actualVersionSplit.length-1; i >=0; i--){
-            if (Integer.parseInt(actualVersionSplit[i]) < Integer.parseInt(minimumVersionSplit[i])){
+            if (Integer.parseInt(actualVersionSplit[i]) < Integer.parseInt(minVersionSplit[i])){
                 return false;
             }
         }
         return true;
+    }
+
+    public static boolean maxVersionSatisfied(String maxVersion, String actualVersion){
+
+        String[] actualVersionSplit = actualVersion.split("\\.");
+        String[] maxVersionSplit = maxVersion.split("\\.");
+
+        if(actualVersionSplit.length != maxVersionSplit.length){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = actualVersionSplit.length-1; i >=0; i--){
+            if (Integer.parseInt(actualVersionSplit[i]) < Integer.parseInt(maxVersionSplit[i])){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean requiredLibrariesAvailable(Map<String, String> requiredLibraries){
@@ -65,7 +120,7 @@ public class PythonDependencyChecker {
             if(availableLibraries.containsKey(lib)){
                 String requiredVersion = requiredLibraries.get(lib);
                 String availableVersion = availableLibraries.get(lib);
-                if(!versionSatisfied(requiredVersion, availableVersion)){
+                if(!minVersionSatisfied(requiredVersion, availableVersion)){
                     // version too low
                     return false;
                 }
@@ -75,5 +130,8 @@ public class PythonDependencyChecker {
             }
         }
         return true;
+    }
+    public static boolean requiredLibrariesAvailable(String requirementsFile){
+        return requiredLibrariesAvailable(readRequirements(requirementsFile));
     }
 }
